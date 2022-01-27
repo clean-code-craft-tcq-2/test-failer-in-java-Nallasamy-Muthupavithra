970543a2cbab444c293ee18ce04969df574c8e04
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-01-27 10:55:09 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,18 +1,45 @@
-public class Misaligned {
-    static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
-            }
-        }
-        return i * j;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class misaligned {
+
+  static int printColorMap() {
+    Map<Integer, String[]> colorPairsMap = getColorPairs();
+    for (Entry<Integer, String[]> colorPairIterator : colorPairsMap.entrySet()) {
+      String[] colorPair = colorPairIterator.getValue();
+      System.out.printf("%d | %s | %s\n", colorPairIterator.getKey(), colorPair[0], colorPair[1]);
     }
-    public static void main(String[] args) { 
-        int result = printColorMap();
-        assert(result == 25);
-        System.out.println("All is well (maybe!)");
+    return colorPairsMap.size();
+  }
+
+  static Map<Integer, String[]> getColorPairs() {
+    String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    Map<Integer, String[]> colorPairs = new HashMap<>();
+    String[] colorPair = new String[2];
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        colorPair[0] = majorColors[i];
+        colorPair[1] = minorColors[i];
+        colorPairs.put((i * 5) + j, colorPair);
+      }
     }
+    return colorPairs;
+  }
+
+  public static void main(final String[] args) {
+    int result = printColorMap();
+    assert (result == 25);
+
+    Map<Integer, String[]> colorPairsMap = getColorPairs();
+    assertEquals("Major colour is equal", "White", colorPairsMap.get(3)[0]);
+    assertEquals("Minor colour is equal", "Green", colorPairsMap.get(3)[1]);
+
+    System.out.println("All is well (maybe!)");
+  }
 }
