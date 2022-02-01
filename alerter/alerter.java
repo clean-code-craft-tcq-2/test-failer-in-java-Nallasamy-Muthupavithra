a094f963f1b78fcf8f90a906a94a6c4080e56b34
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-01 22:08:59 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -1,46 +1,54 @@
 public class Alerter {
-    static int alertFailureCount = 0;
-    static int networkAlertStub(float celcius) {
-        System.out.println("ALERT: Temperature is " + celcius + " celcius");
-        // Return 200 for ok
-        // stub always succeeds and returns 200
-        return 200;
+   static int alertFailureCount = 0;
+
+  static int networkAlertStub(final float celcius) {
+    System.out.println("ALERT: Temperature is " + celcius + " celcius");
+    // Return 200 for ok
+    // stub always succeeds and returns 200
+    return 200;
+  }
+
+  static int networkNotOkStub(final float celcius) {
+    System.out.println("Temperature in Network - 'Not Ok' is " + celcius + " celcius");
+    // Return 500 for not-ok
+    // stub always succeeds and returns 500
+    return 500;
+  }
+
+  static void alertInCelcius(final float farenheit, final String networkTempStatus) {
+    float celcius = convertFarenheitToCelcius(farenheit);
+    int returnCode = resolveNetworkAlert(networkTempStatus, celcius);
+    incrementFailureCount(returnCode);
+  }
+
+  static int resolveNetworkAlert(final String networkTempStatus, final float celcius) {
+    return networkTempStatus == "ok" ? networkAlertStub(celcius) : networkNotOkStub(celcius);
+  }
+
+  static float convertFarenheitToCelcius(final float farenheit) {
+    return ((farenheit - 32) * 5) / 9;
+  }
+
+  static void incrementFailureCount(final int returnCode) {
+    if (returnCode != 200) {
+      alertFailureCount += 1;
     }
-    
-    static int networkNotOkStub(float celcius) {
-        System.out.println("Temperature in Network - 'Not Ok' is " + celcius + " celcius");
-        // Return 500 for not-ok
-        // stub always succeeds and returns 500
-        return 500;
-    }
-    static void alertInCelcius(float farenheit, String networkTempStatus) {
-        float celcius = convertFarenheitToCelcius(farenheit);
-        int returnCode =resolveNetworkAlert(networkTempStatus);
-        incrementFailureCount(returnCode);
-    }
-    static int resolveNetworkAlert(String networkTempStatus){
-        return networkTempStatus=="ok" ? networkAlertStub(celcius) : networkNotOkStub(celcius);
-      }
-    static float convertFarenheitToCelcius(float farenheit){
-       return  (farenheit - 32) * 5 / 9;
-    }
-    static void incrementFailureCount(String returnCode){
-        if (returnCode != 200) {
-            alertFailureCount += 1;
-        }
-    }
-    public static void main(String[] args) {
-        alertInCelcius(400.5f,"ok");
-        assert( 0==alertFailureCount);
-        alertInCelcius(303.6f,"not ok");
-        assert(1==alertFailureCount);
-        System.out.printf("%d alerts failed.\n", alertFailureCount);
-        assert(resolveNetworkAlert("ok")==200);
-        assert(resolveNetworkAlert("not ok")==500);
-        assert(convertFarenheitToCelcius(100)==37.78);
-        assert(incrementFailureCount(200)==2);
-        assert(incrementFailureCount(500)==2);
-        
-        System.out.println("All is well (maybe!)\n");
-    }
+  }
+
+  public static void main(final String[] args) {
+    alertInCelcius(400.5f, "ok");
+    assert (0 == alertFailureCount);
+    alertInCelcius(303.6f, "not ok");
+    assert (1 == alertFailureCount);
+    System.out.printf("%d alerts failed.\n", alertFailureCount);
+    assert (resolveNetworkAlert("ok", 38) == 200);
+    assert (resolveNetworkAlert("not ok", 98) == 500);
+    assert (convertFarenheitToCelcius(100) == 37.78);
+    incrementFailureCount(200);
+    assert (2 == alertFailureCount);
+    incrementFailureCount(500);
+    assert (2 == alertFailureCount);
+
+    System.out.println("All is well (maybe!)\n");
+  }
 }
