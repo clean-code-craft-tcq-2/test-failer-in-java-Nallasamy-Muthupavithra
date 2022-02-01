db8df0b99fa84a5f2782a4ca76f3343598037693
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-01 19:51:18 +0530

    updated to pass the failed tests

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -3,26 +3,28 @@ public class Alerter {
     static int networkAlertStub(float celcius) {
         System.out.println("ALERT: Temperature is " + celcius + " celcius");
         // Return 200 for ok
-        // Return 500 for not-ok
         // stub always succeeds and returns 200
         return 200;
     }
-    static void alertInCelcius(float farenheit) {
+    
+    static int networkNotOkStub(float celcius) {
+        System.out.println("Temperature in Network - 'Not Ok' is " + celcius + " celcius");
+        // Return 500 for not-ok
+        // stub always succeeds and returns 500
+        return 500;
+    }
+    static void alertInCelcius(float farenheit, String networkTempStatus) {
         float celcius = (farenheit - 32) * 5 / 9;
-        int returnCode = networkAlertStub(celcius);
+        int returnCode = networkTempStatus=="ok"?networkAlertStub(celcius):networkNotOkStub(float celcius);
         if (returnCode != 200) {
-            // non-ok response is not an error! Issues happen in life!
-            // let us keep a count of failures to report
-            // However, this code doesn't count failures!
-            // Add a test below to catch this bug. Alter the stub above, if needed.
-            alertFailureCount += 0;
+            alertFailureCount += 1;
         }
     }
     public static void main(String[] args) {
-        alertInCelcius(400.5f);
+        alertInCelcius(400.5f,"ok");
+        assertEquals("Failures count is not right", 0,alertFailureCount);
+        alertInCelcius(303.6f,"not ok");
         assertEquals("Failures count is not right", 1,alertFailureCount);
-        alertInCelcius(303.6f);
-        assertEquals("Failures count is not right", 2,alertFailureCount);
         System.out.printf("%d alerts failed.\n", alertFailureCount);
         System.out.println("All is well (maybe!)\n");
     }
