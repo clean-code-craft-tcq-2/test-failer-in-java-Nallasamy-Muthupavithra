f21d4f274406904058f2f175046bf02d4750b246
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-01 19:56:09 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -7,7 +7,7 @@ public class Tshirts {
         } else if(cms > 40 && cms <=42 {
             return "L";
         } else if(cms <= 36 || cms > 42){
-            return "NA"
+            return "NA";
         }           
     }
 
