2ec93f85505ded01c3d63231b9e0e8a3704b2c26
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-01 21:37:48 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,43 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 class misaligned {
 
-  static int printColorMap() {
-    Map<Integer, String[]> colorPairsMap = getColorPairs();
-    for (Entry<Integer, String[]> colorPairIterator : colorPairsMap.entrySet()) {
-      String[] colorPair = colorPairIterator.getValue();
-      System.out.printf("%d | %s | %s\n", colorPairIterator.getKey(), colorPair[0], colorPair[1]);
-    }
-    return colorPairsMap.size();
-  }
+  static String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
+  static String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
-  static Map<Integer, String[]> getColorPairs() {
-    String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-    Map<Integer, String[]> colorPairs = new HashMap<>();
-    String[] colorPair = new String[2];
-    int i = 0;
-    int j = 0;
+  static void printColorMap() {
+    int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
       for (j = 0; j < 5; j++) {
-        colorPair[0] = majorColors[i];
-        colorPair[1] = minorColors[j];
-        colorPairs.put((i * 5) + j, colorPair);
+        System.out.println(formatColorPair(i, j));
       }
     }
-    return colorPairs;
+  }
+
+  private static String formatColorPair(final int i, final int j) {
+    int pairNumber = formPairNumber(i, j);
+    return pairNumber + "\t" + majorColors[i] + "\t" + minorColors[j] + "\n";
+  }
+
+  private static int formPairNumber(final int i, final int j) {
+    return (i * 5) + j;
   }
 
   public static void main(final String[] args) {
-    int result = printColorMap();
-    assert (result == 25);
-
-    Map<Integer, String[]> colorPairsMap = getColorPairs();
-    assert("White"==colorPairsMap.get(3)[0]);
-    assert("Green"== colorPairsMap.get(3)[1]);
-
+    assert (24 == formPairNumber(4, 4));
+    assert (formatColorPair(0, 0).equals("0\tWhite\tBlue"));
+    
+    printColorMap();
     System.out.println("All is well (maybe!)");
   }
 }
